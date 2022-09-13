bd4d4f97d6f7ac6600cffaf43bdf56054d4707b9
Semenov <semenov@mail.ru>
2022-09-13 20:00:16 +0400

    BD-18 сущность config.screen

---
+++ config/screen.sql
@@ -0,0 +1,13 @@
+/** Create table */
+CREATE TABLE config."screen" (
+    id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+    "name" varchar NOT NULL,
+    description text NULL,
+    title varchar,
+    url varchar,
+    icons - varchar,
+    active boolean DEFAULT true,
+    id_right int,
+    CONSTRAINT screen_fk FOREIGN KEY (id_right) REFERENCES tec.right(id),
+    CONSTRAINT event_pk PRIMARY KEY (id)
+);

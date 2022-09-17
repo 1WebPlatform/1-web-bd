8ea4f997d6222bcb44f1163803df0c7c0dfada20
Semenov <semenov@mail.ru>
2022-09-17 18:06:23 +0400

    BD-18 сущность config.screen

--- config/screen.sql
+++ config/screen.sql
@@ -1,12 +1,12 @@
 CREATE TABLE config."screen"(
-   id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
-   name varchar NOT NULL,
-   description text NULL,
-   title varchar,
-   url varchar,
-   icons varchar,
-   active boolean DEFAULT true,
-   id_right int NULL,
+   "id" int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+   "name" varchar NOT NULL,
+   "description" text NULL,
+   "title" varchar,
+   "url" varchar,
+   "icons" varchar,
+   "active" boolean DEFAULT true,
+   "id_right" int NULL,
    CONSTRAINT screen_fk FOREIGN KEY (id_right) REFERENCES tec.right(id),
    CONSTRAINT screen_pk PRIMARY KEY (id)
 );
@@ -20,7 +20,7 @@ RETURNS SETOF config.screen
 LANGUAGE plpgsql
 AS $function$
    BEGIN
-   return query EXECUTE (select * from  tec.table_get('select * from config.screen', _limit, _offset, _order_by, _where));
+       return query EXECUTE (select * from  tec.table_get('select * from config.screen', _limit, _offset, _order_by, _where));
    END;
 $function$;
 
@@ -46,17 +46,6 @@ AS $function$
    END;
 $function$;
 
-CREATE OR REPLACE FUNCTION config.screen_check_right(
-   _id int
-)
-RETURNS boolean 
-LANGUAGE plpgsql
-AS $function$
-   BEGIN
-      return EXISTS (select * from tec.right where "id_right" = _id);
-   END;
-$function$;
-
 CREATE OR REPLACE FUNCTION config.screen_save(
    _name varchar,
    _description text,
@@ -75,7 +64,7 @@ AS $function$
            return;
        end if;
        INSERT INTO config.screen
-           (name,description,title,url,icons,id_right)
+           ("name","description","title","url","icons","id_right")
            VALUES  (_name,_description,_title,_url,_icons,_id_right)
            RETURNING id INTO id_; 
    END;
@@ -89,15 +78,16 @@ CREATE OR REPLACE FUNCTION config.screen_delete(
 LANGUAGE plpgsql
 AS $function$
    BEGIN
-      if (select * from config.screen_check_id) then 
+      if (select * from config.screen_check_id(_id)) then 
            DELETE FROM config.screen  where id = _id RETURNING id INTO id_;
       else
-      select * from tec.errors_get_id(21);
+      select * into error_ from tec.error_get_id(21);
       end if;
    END;
 $function$;
 
 CREATE OR REPLACE FUNCTION config.screen_update(
+   _id int, 
    _name varchar,
    _description text,
    _title varchar,
@@ -110,7 +100,7 @@ CREATE OR REPLACE FUNCTION config.screen_update(
 LANGUAGE plpgsql
 AS $function$
    BEGIN
-      if (select * from config.screen_check_id) then 
+      if (select * from config.screen_check_id(_id)) then 
            select * into error_ from tec.error_get_id(21);
            return;
       end if;
@@ -119,12 +109,12 @@ AS $function$
          return;
      end if;
        UPDATE  config.screen SET
-        name = _name,
-        description = _description,
-        title = _title,
-        url = _url,
-        icons = _icons,
-        id_right = _id_right
+        "name" = _name,
+        "description" = _description,
+        "title" = _title,
+        "url" = _url,
+        "icons" = _icons,
+        "id_right" = _id_right
        RETURNING id INTO id_; 
    END;
 $function$;

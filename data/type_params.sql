8532dfb517a0e61d172070027708aaf7a17eb1bb
Semenov <semenov@mail.ru>
2022-09-23 21:48:08 +0400

    BD-4 заполнения данными сущности

---
+++ data/type_params.sql
@@ -0,0 +1,4 @@
+INSERT INTO handbook.type_params ("name", description, active) VALUES('input', 'поле ввода строки', true);
+INSERT INTO handbook.type_params ("name", description, active) VALUES('number', 'поле ввода числа', true);
+INSERT INTO handbook.type_params ("name", description, active) VALUES('select', 'поле выпадающий список', true);
+INSERT INTO handbook.type_params ("name", description, active) VALUES('checkbox', 'поле ввода булевого значения', true);

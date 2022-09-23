05c9e85b203415563ee6c18f4414ecb53cb9e00d
Semenov <semenov@mail.ru>
2022-09-23 19:47:20 +0400

    BD-4 заполнения данными сущности

---
+++ data/type_component-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO handbook.type_component ("name", description, active) VALUES('table', 'Таблица', true);
+INSERT INTO handbook.type_component ("name", description, active) VALUES('button', 'Кнопка', true);
+INSERT INTO handbook.type_component ("name", description, active) VALUES('form', 'Форма', true);
+INSERT INTO handbook.type_component ("name", description, active) VALUES('container', 'Контейнер', true);

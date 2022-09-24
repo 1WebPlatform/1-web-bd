9809ff63e1b542d42526cf2e261ae69f949a598b
Semenov <semenov@mail.ru>
2022-09-24 21:40:22 +0400

    BD-4 заполнения данными сущности

--- data/callback-data.sql
+++ data/callback-data.sql
@@ -1,1 +1,2 @@
 INSERT INTO config.callback ("name", description, params, id_type) VALUES('loader_table_data', 'Загружает данные в компонент таблицу.', '{"body":{"type": "json"}, "name":{"type":"string"}, "schema": {"type":"string"}}'::json, 1);
+INSERT INTO config.callback ("name", description, params, id_type) VALUES('api_procedures', 'Вызов api procedures', '{"body":{"type": "json"}, "name":{"type":"string"}, "schema": {"type":"string"}}'::json, NULL);

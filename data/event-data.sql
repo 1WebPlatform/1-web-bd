0e266ceb6dab2b63ea8ceb2aa980968be2b58670
Semenov <semenov@mail.ru>
2022-09-24 18:45:23 +0400

    BD-4 заполнения данными сущности

---
+++ data/event-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO handbook."event" (name, description, active) VALUES('click', 'Событие нажатие кнопки мыши', true);
+INSERT INTO handbook."event" (name, description, active) VALUES('create', 'Событие отрисовки компонента', true);
+INSERT INTO handbook."event" (name, description, active) VALUES('focus', 'Событие фокус компонента', true);
+INSERT INTO handbook."event" (name, description, active) VALUES('hover', 'Событие наведения на компонент', false);

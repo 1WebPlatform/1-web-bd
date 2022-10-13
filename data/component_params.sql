501adc43b8dd5e528525ae649d9a34186e2924e7
Semenov <semenov@mail.ru>
2022-10-13 10:21:10 +0400

    BD-4 заполнения данными сущности

---
+++ data/component_params.sql
@@ -0,0 +1,1 @@
+INSERT INTO config.component_params (id_component, id_params) VALUES(1, 1);
--- data/params-data.sql
+++ data/params-data.sql
@@ -2,3 +2,4 @@ INSERT INTO config.params ("name", description, "select", id_type) VALUES('id_co
 INSERT INTO config.params ("name", description, "select", id_type) VALUES('icons', 'Ссылка на изображения', NULL, 5);
 INSERT INTO config.params ("name", description, "select", id_type) VALUES('text', 'Текст который вставляется в компонент', NULL, 1);
 INSERT INTO config.params ("name", description, "select", id_type) VALUES('title', 'Выплывающая подсказка у компонента', NULL, 1);
+INSERT INTO config.params ("name", description, "select", id_type) VALUES('name', 'Заголовок у компонента', NULL, 1);

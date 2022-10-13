aa13dfed03a8d1ff5f25d59e1fbc9de8696e9d72
Semenov <semenov@mail.ru>
2022-10-13 16:13:00 +0400

    BD-4 заполнения данными сущности

--- data/component_params.sql
+++ data/component_params.sql
@@ -1,1 +1,3 @@
 INSERT INTO config.component_params (id_component, id_params) VALUES(1, 1);
+INSERT INTO config.component_params (id_component, id_params) VALUES(6, 6);
+INSERT INTO config.component_params (id_component, id_params) VALUES(7, 6);
--- data/params-data.sql
+++ data/params-data.sql
@@ -3,3 +3,5 @@ INSERT INTO config.params ("name", description, "select", id_type) VALUES('icons
 INSERT INTO config.params ("name", description, "select", id_type) VALUES('text', 'Текст который вставляется в компонент', NULL, 1);
 INSERT INTO config.params ("name", description, "select", id_type) VALUES('title', 'Выплывающая подсказка у компонента', NULL, 1);
 INSERT INTO config.params ("name", description, "select", id_type) VALUES('name', 'Заголовок у компонента', NULL, 1);
+INSERT INTO config.params (name, description, "select", id_type) VALUES('name_ok', 'Текст у кнопки соглашения у компонента сообщение', NULL, 1);
+INSERT INTO config.params (name, description, "select", id_type) VALUES('name_cancel', 'Текст у кнопки отказ у компонента сообщение', NULL, 1);

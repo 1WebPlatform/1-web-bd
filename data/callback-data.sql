fa769144997cff4f779754b19d406de1191cbf36
Semenov <semenov@mail.ru>
2022-10-14 10:09:42 +0400

    BD-4 заполнения данными сущности

--- data/callback-data.sql
+++ data/callback-data.sql
@@ -1,2 +1,3 @@
 INSERT INTO config.callback ("name", description, params, id_type) VALUES('loader_table_data', 'Загружает данные в компонент таблицу.', '{"body":{"type": "json"}, "name":{"type":"string"}, "schema": {"type":"string"}}'::json, 1);
 INSERT INTO config.callback ("name", description, params, id_type) VALUES('api_procedures', 'Вызов api procedures', '{"body":{"type": "json"}, "name":{"type":"string"}, "schema": {"type":"string"}}'::json, NULL);
+INSERT INTO config.callback ("name", description, params, id_type) VALUES('open_message', 'Открыть компонент message', '{"id": "number"}'::json, 6);
--- data/event-data.sql
+++ data/event-data.sql
@@ -2,3 +2,5 @@ INSERT INTO handbook."event" (name, description, active) VALUES('click', 'Соб
 INSERT INTO handbook."event" (name, description, active) VALUES('create', 'Событие отрисовки компонента', true);
 INSERT INTO handbook."event" (name, description, active) VALUES('focus', 'Событие фокус компонента', true);
 INSERT INTO handbook."event" (name, description, active) VALUES('hover', 'Событие наведения на компонент', false);
+INSERT INTO handbook."event" ("name", description, active) VALUES('click_ok', 'Событие нажатие кнопки Ок в компоненте message', true);
+INSERT INTO handbook."event" ("name", description, active) VALUES('click_cancel', 'Событие нажатие кнопки Отмена в компоненте message', true);
